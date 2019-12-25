598512883f1253316902c13b2976e94e29b59013
martentamm <marten.tamm@gmail.com>
2019-12-25 18:59:15 +0200

    Updates

---
+++ precache-manifest.dbd2d504f4fc5b1a6daf0e99772e7057.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f6abeb74174bbd555b73090e6c4028",
+    "url": "/sl-news/index.html"
+  },
+  {
+    "revision": "6dd51eab6c183d42b56d",
+    "url": "/sl-news/static/css/main.97feac97.chunk.css"
+  },
+  {
+    "revision": "2e21cfec23470ac7eb1b",
+    "url": "/sl-news/static/js/2.8be6820d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/sl-news/static/js/2.8be6820d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6dd51eab6c183d42b56d",
+    "url": "/sl-news/static/js/main.ba715e74.chunk.js"
+  },
+  {
+    "revision": "22ec41cc07ba7bdcf6b7",
+    "url": "/sl-news/static/js/runtime-main.fae16e0d.js"
+  }
+]);
